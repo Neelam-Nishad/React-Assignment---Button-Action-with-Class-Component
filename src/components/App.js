58812c0364db048075d5e2769e348d99181e8070
Neelam-Nishad <73290803+Neelam-Nishad@users.noreply.github.com>
2021-02-14 02:49:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,17 @@ class App extends Component {
     constructor(props) {
 		super(props);
 	};
+	
+	let temp = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+			<button id="click" onClick={ () => {
+						render(){
+							<p id="para">{temp}</p>
+						}	
+			} }>click</button>
     		</div>
     	);
     }
