361b80f112008084f3a811d0beb842a46d9b8767
Neelam-Nishad <73290803+Neelam-Nishad@users.noreply.github.com>
2021-02-28 19:20:11 +0530

    Update App.js
    #3

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,21 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { paragraph: "" };
+	this.state = { paragraph: "" };
+	this.show = this.show.bind(this);
   }
 
   show() {
     this.setState({
-      paragraph: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+      paragraph:
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
     });
   }
   render() {
     return (
       <div id="main">
         <p id="para">{this.state.paragraph}</p>
-        <button id="click" onClick={this.show()}>
+        <button id="click" onClick={this.show}>
           click
         </button>
       </div>
